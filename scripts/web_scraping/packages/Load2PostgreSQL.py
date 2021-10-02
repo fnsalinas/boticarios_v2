9e70c979852e0c91167c0a6760de67056692b1ea
Fabio Nelson Salinas Moreno <39883976+fnsalinas@users.noreply.github.com>
2021-10-02 12:09:37 -0500

    Updates

--- scripts/web_scraping/packages/Load2PostgreSQL.py
+++ scripts/web_scraping/packages/Load2PostgreSQL.py
@@ -1,1 +1,16 @@
 from scripts.web_scraping.packages.runSQL import runSQL
+from scripts.web_scraping.database.create_tables import colsubsidio
+
+class sqlColsubsidio():
+
+    def createColsubsidio(): 
+        print(colsubsidio.getSQLCreate())
+    
+    def insertColsubsidio():
+        print("Pendiente")
+    
+    def deleteColsubsidio():
+        print("Pendiente")
+    
+    def dropColsubsidio():
+        print("Pendiente")
